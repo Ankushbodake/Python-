c310a4ddf0d134253c4acbdc6cca2437ff45e0c5
Ankush <>
2023-07-26 22:11:25 +0530

    second commit

---
+++ notebook.py
@@ -0,0 +1,7 @@
+import pandas as pandas
+import numpy as numpy
+import seaborn as sns
+import matplotlib.pyplot as pyplot
+from sklearn.preprocessing import StandardScaler,OneHotScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline ,make_pipeline
